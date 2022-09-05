22a6357963d1b7be0ac56533debc95206c287b72
delia850331 <delia850331@gmail.com>
2022-09-05 23:31:42 +0800

    刪除不必要檔案

--- HomeWork/HomeWork/Form1.Designer.cs
+++ HomeWork/HomeWork/Form1.Designer.cs
@@ -65,7 +65,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(418, 56);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Homework3";
+            this.button2.Text = "Homework3(找不到資料庫，無法啟動)";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
